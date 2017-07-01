67db23a7b6d6559ad5d78a6da5805a42897c9866
cash <cash@cashwu.com>
2017-07-01 13:38:58 +0800

    BudgetController 第一個紅燈

---
+++ GOOS_SampleTests/steps/BudgetControllerSteps.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using FluentAssertions;
+using GOOS_Sample.Controllers;
+using GOOS_Sample.Models.ViewModels;
+using GOOS_SampleTests.DataModelsForTest;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace GOOS_SampleTests.steps
+{
+    [Binding]
+    public class BudgetControllerSteps
+    {
+        private BudgetController _budgetController;
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            this._budgetController = new BudgetController();
+        }
+
+        [When(@"add a budget")]
+        public void WhenAddABudget(Table table)
+        {
+            var model = table.CreateInstance<BudgetAddViewModel>();
+            var result = _budgetController.Add(model);
+            ScenarioContext.Current.Set<ActionResult>(result);
+        }
+        
+        [Then(@"ViewBag should have a message for adding successfully")]
+        public void ThenViewBagShouldHaveAMessageForAddingSuccessfully()
+        {
+            var result = ScenarioContext.Current.Get<ActionResult>() as ViewResult;
+            string message = result.ViewBag.Message;
+            message.Should().Be(GetAddingSuccessfullyMessage());
+        }
+
+        private static string GetAddingSuccessfullyMessage()
+        {
+            return "added successfully";
+        }
+
+        [Then(@"it should exist a budget record in budget table")]
+        public void ThenItShouldExistABudgetRecordInBudgetTable(Table table)
+        {
+            using (var db = new NorthwindEntities())
+            {
+                var budgrt = db.Budgets.FirstOrDefault();
+                budgrt.Should().NotBeNull();
+                table.CompareToInstance(budgrt);
+            }
+        }
+    }
+}
